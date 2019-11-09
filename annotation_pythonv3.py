6f2cd51de08f9d7c1202a8226cc96e271618c7c7
Sabrine <sabrine.riahi@student.umons.ac.be>
2019-11-09 16:09:53 +0100

    annotation_pythonv3 changes

--- annotation_pythonv3.py
+++ annotation_pythonv3.py
@@ -7,6 +7,19 @@ tab = []
 
 k = 0
 
+def convert(size, box):
+    dw = 1./size[0]
+    dh = 1./size[1]
+    x = (box[0] + box[1])/2.0
+    y = (box[2] + box[3])/2.0
+    w = box[1] - box[0]
+    h = box[3] - box[2]
+    x = x*dw
+    w = w*dw
+    y = y*dh
+    h = h*dh
+    return (x,y,w,h)
+
 while k <= len(line) :
     for i in line :
         newline = i.replace (",", " ")
@@ -41,16 +54,3 @@ while i <= len(line) :
     i+=1
     
 file.close()
-
-def convert(size, box):
-    dw = 1./size[0]
-    dh = 1./size[1]
-    x = (box[0] + box[1])/2.0
-    y = (box[2] + box[3])/2.0
-    w = box[1] - box[0]
-    h = box[3] - box[2]
-    x = x*dw
-    w = w*dw
-    y = y*dh
-    h = h*dh
-    return (x,y,w,h)
